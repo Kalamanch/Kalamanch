0b43d624b78714152a1720be45c4b1e0f5a2c693
Aaditya <aadityanitin@gmail.com>
2022-04-28 23:37:23 +0530

    feat: add next app and login page

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['mdbcdn.b-cdn.net'],
+    },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig 
